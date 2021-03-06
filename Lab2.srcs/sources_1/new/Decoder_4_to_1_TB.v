35d2a1208800e5db3c3f49c4b5d3e3f16957a233
Saeed Akbar Khan <79642603+Saeed-Akbar0020@users.noreply.github.com>
2021-03-06 12:50:25 +0500

    Add files via upload

---
+++ Lab2.srcs/sources_1/new/Decoder_4_to_1_TB.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/06/2021 12:02:56 PM
+// Design Name: 
+// Module Name: Decoder_4_to_1_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder_4_to_1_TB();
+        reg EN, A, B;
+        wire [3:0] D;
+
+    
+    initial 
+    begin 
+    #00 EN=1'b1; A=1'bx;B=1'bx;
+    #10 EN=1'b0; A=1'b0;B=1'b0;
+    #10 EN=1'b0; A=1'b0;B=1'b1;
+    #10 EN=1'b0; A=1'b1;B=1'b0;
+    #10 EN=1'b0; A=1'b1;B=1'b1;
+    #10 $stop;
+    end
+    // display on console
+    initial 
+    $monitor($time, " ns -- EN =%b, A = %b, B = %b -- D =%b", EN, A, B, D);
+     
+     // instantiation
+    Decoder_4_to_1 inst0(A, B, EN, D);
+    
+endmodule
